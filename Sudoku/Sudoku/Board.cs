3b9721b7b54985434b57e3a046d50d713479aef9
Vlad <Vhnes02@gmail.com>
2017-04-10 21:02:49 +0300

    Write Board constructor.

--- Sudoku/Sudoku/Board.cs
+++ Sudoku/Sudoku/Board.cs
@@ -5,8 +5,104 @@ namespace Sudoku
 {
     public class Board
     {
-        List<List<Cell>> cells;
+        List<List<Cell>> cells = new List<List<Cell>>(9);
+        public Board()
+        {
+            initializeBoard();
+            setInitialCells();
+        }
 
+        private void initializeBoard()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                cells.Add(new List<Cell>(9));
+                for (int j = 0; j < 9; j++)
+                {
+                    cells[i].Add(new MutableCell(i, j, 0));
+                }
+            }
+
+        }
+
+        private void setInitialCells()
+        {
+            for (int i = 0; i < 36; i++)
+            {
+                setRandomCell();
+            }
+        }
+
+        private void setRandomCell()
+        {
+            Random rand = new Random();
+            int row = rand.Next(0, 9);
+            int column = rand.Next(0, 9);
+            int value = rand.Next(1, 10);
+            if (checkAmount(row, column))
+            {
+                Cell cell = new ImmutableCell(row, column, value);
+                cells[row][column] = cell;
+            }
+            else
+            {
+                setRandomCell();
+            }
+        }
+
+        private bool checkAmount(int row, int column)
+        {
+            bool rowBool = checkRowAmount(row);
+            bool columnBool = checkColumnAmount(column);
+            bool squareBool = checkSquareAmount(row, column);
+            return rowBool && columnBool && squareBool;
+        }
+
+        private bool checkRowAmount(int row)
+        {
+            int amount = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                if (cells[row][i].Value != 0)
+                {
+                    amount++;
+                }
+            }
+            return amount < 5;
+
+        }
+
+        private bool checkColumnAmount(int column)
+        {
+            int amount = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                if (cells[i][column].Value != 0)
+                {
+                    amount++;
+                }
+            }
+            return amount < 5;
+        }
+
+        private bool checkSquareAmount(int row, int column)
+        {
+            int i = 0;
+            int squareRow = 3 * (row / 3);
+            int squareColumn = 3 * (column / 3);
+            for (int rowCounter = squareRow; rowCounter < squareRow + 3; rowCounter++)
+            {
+                for (int columnCounter = squareColumn; columnCounter < squareColumn + 3; columnCounter++)
+                {
+                    if (cells[rowCounter][columnCounter].Value != 0)
+                    {
+                        i++;
+                    }
+                }
+            }
+            return i < 5;
+        } 
+ 
         public void setValue(int row, int column, int value)
         {
             checkInput(row);
